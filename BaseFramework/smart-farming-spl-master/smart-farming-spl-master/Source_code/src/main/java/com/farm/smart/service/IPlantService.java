68a92e9be698f001663fb4aaac81660231f31e3d
Edwin Gómez <edagomez@bancolombia.com.co>
2019-11-03 17:11:13 -0500

    delete BaseFramework

--- BaseFramework/smart-farming-spl-master/smart-farming-spl-master/Source_code/src/main/java/com/farm/smart/service/IPlantService.java
+++
@@ -1,9 +0,0 @@
-package com.farm.smart.service;
-
-import com.farm.smart.entities.Plant;
-
-import java.util.List;
-
-public interface IPlantService {
-    List<Plant> findAll();
-}
--- BaseFramework/smart-farming-spl-master/smart-farming-spl-master/src/main/java/com/farm/smart/rest/AuthResource.java
+++
@@ -1,7 +0,0 @@
-package com.farm.smart.rest;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AuthResource {
-}
